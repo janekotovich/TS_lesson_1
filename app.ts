7070bdd3dd5c3995f5f830508e91cbf845735af3
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-05 16:43:48 +1000

    TypeScript lesson1. Learning types.

---
+++ app.ts
@@ -0,0 +1,9 @@
+const addNumbers = function (n1: number, n2: number) {
+  return n1 + n2;
+};
+
+const number1 = 5;
+const number2 = 5.8;
+
+const result = addNumbers(number1, number2);
+console.log(result);
