af76fadfc4b72679b059d26d7d3a670f3863ea36
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-06 16:25:08 +1000

    Lesson 4 - Array types.

--- app.ts
+++ app.ts
@@ -5,9 +5,14 @@
 const person = {
   name: "Janey",
   age: 32,
+  hobbies: ["Sports", "Cooking"],
 };
 
 console.log(person);
+let favActivities: (string | number)[];
+favActivities = ["Sport", 2];
+let anotherHobbies: any[];
+anotherHobbies = ["fishing", "hiking", "surfing", false, true, 1, 12, "123"];
 
 const product: {
   id: string;
@@ -27,3 +32,4 @@ const product: {
   },
 };
 console.log(product.tags[1]);
+person.hobbies.forEach((hobbie) => console.log(hobbie.toUpperCase()));
