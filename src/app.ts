6f8a0bb92b1a422909631a6f49706d1ea86b8e64
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-23 15:58:14 +1000

    working with strictNullChecks

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ if (typeof userInput === "string") {
   userName = userInput;
 }
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-let result = generateError("An error occured", 700);
-console.log(result);
+// function generateError(message: string, code: number): never {
+//   throw { message: message, errorCode: code };
+// }
+// let result = generateError("An error occured", 700);
+// console.log(result);
