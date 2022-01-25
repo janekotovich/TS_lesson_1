2e84fac48299414cfb6fd97c9bd28a7a7f7eddc0
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-25 18:55:11 +1000

    Classes in TS
    private fieldds in TS

--- src/classes.ts
+++ src/classes.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
   constructor(n: string) {
     this.name = n;
     this.describe();
@@ -7,11 +8,22 @@ class Department {
   describe(this: Department) {
     console.log(`Department ${this.name}`);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeesInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const marketing = new Department("Marketing");
 console.log(marketing);
 
 marketing.describe();
-const marketingCopy = { name: "S", describe: marketing.describe };
-marketingCopy.describe();
+// const marketingCopy = { name: "S", describe: marketing.describe };
+// marketingCopy.describe();
+marketing.addEmployee("James");
+marketing.addEmployee("Jackson");
+marketing.describe();
+marketing.printEmployeesInfo();
