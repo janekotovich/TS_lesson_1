3e4253e811698d7df5ca1ea9d7032726d80790c6
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-25 18:47:13 +1000

    Classes in TS
    classes and this - catching missing this

--- src/classes.ts
+++ src/classes.ts
@@ -2,8 +2,16 @@ class Department {
   name: string;
   constructor(n: string) {
     this.name = n;
+    this.describe();
+  }
+  describe(this: Department) {
+    console.log(`Department ${this.name}`);
   }
 }
 
 const marketing = new Department("Marketing");
 console.log(marketing);
+
+marketing.describe();
+const marketingCopy = { name: "S", describe: marketing.describe };
+marketingCopy.describe();
