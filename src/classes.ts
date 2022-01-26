26ab2645031f3074765b325bd0cad8425e6e68da
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-26 12:50:25 +1000

    Classess in TS
    Getters and setters

--- src/classes.ts
+++ src/classes.ts
@@ -43,8 +43,37 @@ class ItDepartment extends Department {
   }
 }
 
+class Accounnting extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) return this.lastReport;
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in correct value");
+    }
+    this.addReport(value);
+  }
+
+  constructor(private reports: string[]) {
+    super("Accounting", "7", "pink");
+    this.lastReport = reports[0];
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+}
+
 const newItD = new ItDepartment("333", "red", ["Bob", "Jack"]);
 console.log(newItD);
 newItD.addEmployee("Buddy");
+const accounting = new Accounnting([]);
+accounting.mostRecentReport = "Year end report";
+console.log(accounting.mostRecentReport);
 newItD.addEmployee("Andrew");
+
 console.log(newItD);
