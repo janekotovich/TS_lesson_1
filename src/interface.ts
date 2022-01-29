02a9efaeaa1c1709a53df348a3bdbedc4bd5e840
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-29 19:11:43 +1000

    Classes and Interface
    Extending interfaces

--- src/interface.ts
+++ src/interface.ts
@@ -1,20 +1,21 @@
 interface Greetable {
-  name: string;
   greet(phrase: string): void;
 }
+interface Named {
+  readonly name: string;
+}
+// let user1: Greetable;
 
-let user1: Greetable;
+// user1 = {
+//   name: "Jane",
+//   greet(phrase: string) {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
 
-user1 = {
-  name: "Jane",
-  greet(phrase: string) {
-    console.log(phrase + " " + this.name);
-  },
-};
+// user1.greet("Phrase is here");
 
-user1.greet("Phrase is here");
-
-class Person implements Greetable {
+class Person implements Greetable, Named {
   name: string;
   constructor(n: string) {
     this.name = n;
@@ -26,3 +27,22 @@ class Person implements Greetable {
 
 let user2 = new Person("James");
 user2.greet("Another greeting");
+// user1.name = "Pit";
+console.log(user2);
+
+interface Apple {
+  apple: string;
+}
+interface Basket extends Apple {
+  basket(fruit: string): void;
+}
+
+class basket1 implements Basket {
+  apple: string;
+  constructor(n: string) {
+    this.apple = n;
+  }
+  basket(fruit: string) {
+    console.log(`This basket has ${this.apple}`);
+  }
+}
