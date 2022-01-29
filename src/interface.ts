4340a5180c250e5ac800f13732d0f384918a66ff
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-29 15:54:54 +1000

    Classes and Interface
    Interface and classes

--- src/interface.ts
+++ src/interface.ts
@@ -1,17 +1,28 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+let user1: Greetable;
 
 user1 = {
   name: "Jane",
-  age: 32,
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
   },
 };
 
 user1.greet("Phrase is here");
+
+class Person implements Greetable {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user2 = new Person("James");
+user2.greet("Another greeting");
