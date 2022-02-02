34ddf31e137841d59b7d8f1bec5b0d9781a1909c
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-02 15:53:20 +1000

    Advanced Types
    Type Guards - typeof, in, instance of

--- src/advancedTypes.ts
+++ src/advancedTypes.ts
@@ -20,3 +20,56 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function addCombinable(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = GeneralEmployee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(`My name IS ${emp.name}`);
+
+  if ("privilages" in emp) {
+    console.log(`Privilages are: ${emp.privilages}`);
+  }
+
+  if ("startDate" in emp) {
+    console.log(`Start date: ${emp.startDate}`);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("Driving..");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a Truck ..");
+  }
+  loadCargo(amount: number) {
+    console.log(`Loading cargo ... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
