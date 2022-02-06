42b3cc8c0b46e0223a6da71fd7163a0e7fbe08da
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-06 19:13:27 +1000

    Generics
    Arrays and Array<string> tyoes

--- src/generics.ts
+++ src/generics.ts
@@ -1,1 +1,5 @@
 const names = ["jane", "james"];
+const array: Array<string> = []; // string[]
+// ts doesn't complain becasue it knows its an array of strings!
+// just privude additional data
+array[0].split(" ");
