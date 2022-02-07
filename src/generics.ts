ee0aed2c5bc428d4341356c73e8f7e442bf6b399
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-07 17:09:32 +1000

    Generics
    Generic functions

--- src/generics.ts
+++ src/generics.ts
@@ -1,16 +1,25 @@
-const names = ["jane", "james"];
-const array: Array<string> = []; // string[]
-// ts doesn't complain becasue it knows its an array of strings!
-// just privude additional data
-// array[0].split(" ");
+// const names = ["jane", "james"];
+// const array: Array<string> = []; // string[]
+// // ts doesn't complain becasue it knows its an array of strings!
+// // just privude additional data
+// // array[0].split(" ");
 
-const promise: Promise<string> = new Promise((res, rej) => {
-  setTimeout(() => {
-    res("This is recolved");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("This is recolved");
+//   }, 2000);
+// });
 
-// you not sure what data - res is , unless you return it with generic type
-promise.then((data) => {
-  data.split(" ");
-});
+// // you not sure what data - res is , unless you return it with generic type
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Jane" }, { age: 33 });
+const mergedObj2 = merge({ name: "Jane", country: "Russia" }, { age: 33 });
+console.log(mergedObj.name);
+console.log(mergedObj2.country);
