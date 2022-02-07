5f2486ecda9c50335fcc04c6867a38e7a95014e2
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-07 16:37:20 +1000

    Generics
    Promise and generics

--- src/generics.ts
+++ src/generics.ts
@@ -2,4 +2,15 @@ const names = ["jane", "james"];
 const array: Array<string> = []; // string[]
 // ts doesn't complain becasue it knows its an array of strings!
 // just privude additional data
-array[0].split(" ");
+// array[0].split(" ");
+
+const promise: Promise<string> = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("This is recolved");
+  }, 2000);
+});
+
+// you not sure what data - res is , unless you return it with generic type
+promise.then((data) => {
+  data.split(" ");
+});
