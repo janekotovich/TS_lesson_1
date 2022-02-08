67f02032a5e0d50142947cd9ae7cfcbe16518df4
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-08 18:00:32 +1000

    Decorators
    More useful decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -22,8 +22,20 @@ function LoggerFactory(logString: string) {
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
 // customizing decorator
-@LoggerFactory("LOGGING PERSON")
+// @LoggerFactory("LOGGING PERSON")
+@WithTemplate("<h1>My person object</h1>", "app")
 class Persons2 {
   name = "Jane";
   constructor() {
