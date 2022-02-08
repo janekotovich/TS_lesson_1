dfd87a88b66e1de5c41fdc0ae6345afbb860a898
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-08 18:07:20 +1000

    Decorators
    Adding multiple decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -3,27 +3,31 @@ function Logger(target: Function) {
   //   console.log(target);
 }
 
-@Logger
-class Persons {
-  name = "Jane";
-  constructor() {
-    console.log("Creating person object...");
-  }
-}
+// @Logger
+// class Persons {
+//   name = "Jane";
+//   constructor() {
+//     console.log("Creating person object...");
+//   }
+// }
 
-const newPerson = new Persons();
+// const newPerson = new Persons();
 
 // console.log(newPerson);
 
 function LoggerFactory(logString: string) {
+  console.log("LOGGER FACTORY");
   return function (target: Function) {
+    console.log("Creating person object...");
     console.log(logString);
     console.log(target);
   };
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE");
   return function (constructor: any) {
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -34,7 +38,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // customizing decorator
-// @LoggerFactory("LOGGING PERSON")
+@LoggerFactory("LOGGING PERSON FUNCTION")
 @WithTemplate("<h1>My person object</h1>", "app")
 class Persons2 {
   name = "Jane";
@@ -44,5 +48,3 @@ class Persons2 {
 }
 
 const newPerson2 = new Persons2();
-
-console.log(newPerson2);
