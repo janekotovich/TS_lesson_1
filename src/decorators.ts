2859f9d8cc70330ae0344864adb64648a99bb96d
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-10 14:44:44 +1000

    Decorators
    Property decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -48,3 +48,30 @@ class Persons2 {
 }
 
 const newPerson2 = new Persons2();
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this.price * (1 + tax);
+  }
+}
